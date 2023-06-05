b1416c1168a72a060d1b35b9da6153998040e61d
Daniel Calderon <daniel.calderon.portillo@gmail.com>
2023-06-05 14:44:29 +0200

    Using createPortal to add the modal into a new root div

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -1,23 +1,36 @@
+import { Fragment } from "react";
+import { createPortal } from "react-dom";
+
 import Button from "./Button.js";
 import "./ErrorModal.css";
 
-const ErrorModal = (props) => {
-  const buttonClickHandler = () => {
-    props.onHide();
-  };
+const Overlay = (props) => {
+  return <div className="modal-overlay" onClick={props.onHide} />;
+};
 
+const Modal = (props) => {
   return (
-    <div className="errorModal">
-      <div className="errorModal__modal">
-        <h2 className="errorModal__title">Invalid input</h2>
-        <div className="errorModal__content">{props.errorMessage}</div>
-        <div className="errorModal__footer">
-          <Button onClick={buttonClickHandler}>
-            Close
-          </Button>
-        </div>
+    <div className="modal modal--error">
+      <h2 className="modal__title">Invalid input</h2>
+      <div className="modal__content">{props.errorMessage}</div>
+      <div className="modal__footer">
+        <Button onClick={props.onHide}>Close</Button>
       </div>
     </div>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <Fragment>
+      {createPortal(
+        <>
+          <Overlay onHide={props.onHide} />
+          <Modal errorMessage={props.errorMessage} onHide={props.onHide} />
+        </>,
+        document.getElementById("modal-root")
+      )}
+    </Fragment>
   );
 };
 
